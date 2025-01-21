3c0448ae6b9ac0080780cf2ecac8e4c947f91986
PirosMacska <140020663+PirosMacska@users.noreply.github.com>
2025-01-21 23:52:38 +0100

    Update weidian.js

--- RepHelper/scripts/platforms/weidian.js
+++ RepHelper/scripts/platforms/weidian.js
@@ -1,7 +1,7 @@
 async function main() {
     if (!this.document.location.href.includes("weidian.com/item.html") || !(this.document.location.href.includes("itemId") || this.document.location.href.includes("itemID"))) return
     if (!(await isOpenAgentEnabled())) return
-    const platform_link = "https://weidian.com/item.html?itemId=" + getQuery().itemId
+    const platform_link = "https://weidian.com/item.html?itemId=" + (getQuery().itemId || getQuery().itemID)
 
     openLinkOnPreferredAgent(platform_link)
 }
