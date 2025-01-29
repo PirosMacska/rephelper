7cd31b5d55c63de751ab67d75ba135f09a3177b5
PirosMacska <140020663+PirosMacska@users.noreply.github.com>
2025-01-30 00:03:21 +0100

    added spinner css

--- sheet/qc/index.js
+++ sheet/qc/index.js
@@ -7,23 +7,69 @@ async function fetchLink(link) {
     return json.data
 }
 
+function replaceQuery(url, key, value) {
+    const query = getQueryFromLink(url)
+    query[key] = value
+    let queryString = "?"
+    const queryKeys = Object.keys(query)
+    console.log(query)
+    for(let i = 0; i < queryKeys.length; i++) {
+        queryString += queryKeys[i]  + "=" + query[queryKeys[i]] + ((queryKeys.length - 1 === i) ? "" : "&" )
+    }
+    return url.split("?")[0] + queryString
+}
 
+async function loadImagesFromLink(link) {
+    document.querySelector("#loading-spinner").style.display = "block"
+    window.history.pushState("", "", replaceQuery(document.location.search, "link", link))
+    const item = await fetchLink(link)
+    while (pictures.firstElementChild) {
+        pictures.removeChild(pictures.firstElementChild)
+    }
+    if (item === null) {
+        const span = document.createElement("span")
+        span.textContent = "Sorry no images found :("
+        span.setAttribute("class", `no-images-txt`)
+        pictures.appendChild(span)
+        document.querySelector("#loading-spinner").style.display = "none"
+        return
+    }
+    for (let i = 0; i < item.images.length; i++) {
+        const img = document.createElement("img")
+        img.src = item.images[i]
+        img.setAttribute("class", `item-img item-img-${i}`)
+        pictures.appendChild(img)
+    }
+    document.querySelector("#loading-spinner").style.display = "none"
+}
 
 search_input.addEventListener("keypress", async (e) => {
-    console.log(e.which)
-    if(e.which === 13) {
-        const item = await fetchLink(search_input.value)
-        while (pictures.firstElementChild) {
-            pictures.removeChild(pictures.firstElementChild)
-        }
-        if(item === null) {
-            return
-        }
-        for(let i = 0; i < item.images.length; i++) {
-            const img = document.createElement("img")
-            img.src = item.images[i]
-            img.setAttribute("class", "item-img")
-            pictures.appendChild(img)
-        }
+    if (e.which === 13) {
+        loadImagesFromLink(search_input.value)
     }
 })
+
+function getQueryFromLink(link) {
+    if (!link.includes("?")) return {}
+    const search = link.split("?")[1]
+    const splitted = search.split("&")
+    let query = {}
+    for (let i = 0; i < splitted.length; i++) {
+        const s = splitted[i].split("=")
+        query[s[0]] = s[1]
+    }
+    return query
+}
+
+function getQuery() {
+    return getQueryFromLink(document.location.href)
+}
+
+(async () => {
+    console.log("now")
+    const query = getQuery()
+    console.log(query)
+    if (query.link) {
+        loadImagesFromLink(query.link)
+    }
+})();
